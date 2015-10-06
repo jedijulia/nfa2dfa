a06efb9ba7f3bf127d289c0188a84db7f963f3ca
Julia Menchavez <jedijulia@users.noreply.github.com>
2015-10-06 21:43:23 +0800

    fixed some linter errors in application js

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,57 +1,59 @@
-//var regex = "b*ab*a(a+b)*"; 
-//var regex = "b*ab*ab*"; 
-//var regex = "b*(ab*ab*)*"; 
-//var regex = "a(a+b)*";
-var regex = "a"; 
+// var regex = "b*ab*a(a+b)*";
+// var regex = "b*ab*ab*";
+// var regex = "b*(ab*ab*)*";
+// var regex = "a(a+b)*";
+var regex = 'a';
 var nfa = RegexParser.parse(regex);
-//NFAVisualizer.visualize('#nfa', nfa);
-//console.log(nfa)
+// NFAVisualizer.visualize('#nfa', nfa);
+// console.log(nfa)
 
-dfa = NFAConverter.convert(nfa);
-console.log(dfa);
+var dfa = NFAConverter.convert(nfa);
+// console.log(dfa);
 NFAVisualizer.visualize('#nfa', dfa);
 
 var events = [];
 dfa.addEventListener('yield', function(e) {
-  events.push(e);
+    events.push(e);
 });
 
-console.log(nfa.accepts('ababaa'));
+// console.log(nfa.accepts('ababaa'));
 
 var previous = null;
 step();
 function step() {
-  if (events.length) {
-    var current = document.querySelector('circle.current');
-    if (current) {
-      previous = events.shift().state;
-      var label = document.querySelector('.labels p.current');
-      var transition = document.querySelectorAll('path.current');
-      current.classList.remove('current');
-      label.classList.remove('current');
-      if (transition) {
-        for (var i = 0; i < transition.length; i++) {
-          transition[i].classList.remove('current');
+    if (events.length) {
+        var current = document.querySelector('circle.current');
+        if (current) {
+            previous = events.shift().state;
+            var label = document.querySelector('.labels p.current');
+            var transition = document.querySelectorAll('path.current');
+            current.classList.remove('current');
+            label.classList.remove('current');
+            if (transition) {
+                for (var i = 0; i < transition.length; i++) {
+                    transition[i].classList.remove('current');
+                }
+            }
         }
-      }
-    }
-    var state = events.shift().state;
-    var label = document.querySelector('.labels p[for="' + state.label + '"]');
-    state = document.querySelector('circle[label="' + state.label + '"]');
-    state.classList.add('current');
-    label.classList.add('current');
-    if (previous) {
-      var source = previous.label;
-      var destination = state.getAttribute('label');
-      var transition = document.querySelectorAll('path[source="' + source + '"][destination="' + destination + '"]');
-      if (transition) {
-        for (var i = 0; i < transition.length; i++) {
-          transition[i].classList.add('current');
+        var state = events.shift().state;
+        var label = document.querySelector('.labels p[for="'
+            + state.label + '"]');
+        state = document.querySelector('circle[label="' + state.label + '"]');
+        state.classList.add('current');
+        label.classList.add('current');
+        if (previous) {
+            var source = previous.label;
+            var destination = state.getAttribute('label');
+            var transition = document.querySelectorAll('path[source="'
+                + source + '"][destination="' + destination + '"]');
+            if (transition) {
+                for (var i = 0; i < transition.length; i++) {
+                    transition[i].classList.add('current');
+                }
+            }
         }
-      }
+        setTimeout(step, 500);
     }
-    setTimeout(step, 500);
-  }
 }
 
 // console.log("out here!");
